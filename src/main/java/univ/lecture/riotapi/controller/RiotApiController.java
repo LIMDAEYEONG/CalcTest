18875d9c993421577589eee4fe354db98f718276
Sanghyeon Lee <isseebx38@gmail.com>
2017-04-11 17:20:17 +0900

    test recover

--- src/main/java/univ/lecture/riotapi/controller/RiotApiController.java
+++ src/main/java/univ/lecture/riotapi/controller/RiotApiController.java
@@ -34,12 +34,9 @@ public class RiotApiController {
     @Value("${riot.api.key}")
     private String riotApiKey;
 
-    @RequestMapping(value = "/calc", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    @RequestMapping(value = "/calc/{name}", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public JSONResult queryResult(@PathVariable("name") String expression) throws UnsupportedEncodingException {
-        final String url = riotApiEndpoint + "/calc/" +
-        		expression +
-                "?api_key=" +
-                riotApiKey;
+        final String url = riotApiEndpoint + "/calc/" + expression;
         final int teamId = 8; //조번호(8조) 
         double mathResult;
         
