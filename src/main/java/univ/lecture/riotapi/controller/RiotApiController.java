2027db75b97807443aa930855868b431e5644439
Lim Dae Yeong <limdae07@gmail.com>
2017-04-11 19:46:58 +0900

    aa

--- src/main/java/univ/lecture/riotapi/controller/RiotApiController.java
+++ src/main/java/univ/lecture/riotapi/controller/RiotApiController.java
@@ -61,10 +61,10 @@ public class RiotApiController {
 		DateFormat dateFormat = new SimpleDateFormat("yyyyMMddkkmm");
 		String strTime = dateFormat.format((Calendar.getInstance()).getTime());
 		long now = Long.parseLong(strTime);
-		System.out.println(now);
-		
+		System.out.println(now);		
 		JSONResult result = new JSONResult(teamId, now, mathResult);
 		
+		System.out.println(response);		
 		String endPointResponse = restTemplate.postForObject(endPointUrl, result, String.class);
 		
         return result;
